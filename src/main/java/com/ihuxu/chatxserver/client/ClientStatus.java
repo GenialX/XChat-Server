f1ff049a83296e4ae10f50da7abb3caa3ce807ae
GenialX <admin@ihuxu.com>
2019-01-29 19:02:00 +0800

    :wrench: client fsm methods

--- src/main/java/com/ihuxu/chatxserver/client/ClientStatus.java
+++ src/main/java/com/ihuxu/chatxserver/client/ClientStatus.java
@@ -1,5 +1,7 @@
 package com.ihuxu.chatxserver.client;
 
+import java.util.ArrayList;
+
 /**
  * 客户端状态类.
  * 
@@ -8,7 +10,18 @@ package com.ihuxu.chatxserver.client;
  * @author GenialX
  */
 public class ClientStatus {
-	public int status;
-	public ClientStatus parent;
-	public ClientStatus[] next;
+	public int status = -1;
+	public ClientStatus parent = null;
+	public ArrayList<ClientStatus> next;
+	
+	public ClientStatus() {}
+
+	public ClientStatus(int status) {
+		this.status = status;
+		this.parent = null;
+	}
+
+	public boolean addNext(ClientStatus e) {
+		return next.add(e);
+	}
 }
